0a33c874828565c8e4a4fdd45b37ce23a1996a76
Harmen <harm@dds.nl>
2014-06-25 22:00:22 +0200

    Add IPv6 support.

--- v_proc.go
+++ v_proc.go
@@ -2,6 +2,7 @@ package procspy
 
 import (
 	"bufio"
+	"encoding/hex"
 	"errors"
 	"fmt"
 	"io"
@@ -24,23 +25,31 @@ func Spy() []ConnProc {
 
 	res := []ConnProc{}
 
-	fh, err := os.Open("/proc/net/tcp")
-	defer fh.Close()
-	if err != nil {
-		fmt.Printf("Open err: %v\n", err)
-	}
-	if err == nil {
-		// fmt.Printf("Go parseTransport4\n")
-		for _, tp := range parseTransport4(fh) {
-			// fmt.Printf("Tp: %+v\n", tp)
-			if pid, ok := inodes[tp.inode]; ok {
-				res = append(res, ConnProc{
-					Protocol:  "tcp",
-					LocalAddr: tp.localAddress,
-					LocalPort: tp.localPort,
-					PID:       pid,
-					Name:      "unknown yet", // <-- todo
-				})
+	for _, procFile := range []string{
+		"/proc/net/tcp",
+		"/proc/net/tcp6",
+	} {
+		fh, err := os.Open(procFile)
+		defer fh.Close()
+		if err != nil {
+			fmt.Printf("Open err: %v\n", err)
+		}
+		if err == nil {
+			for _, tp := range parseTransport(fh) {
+				if pid, ok := inodes[tp.inode]; ok {
+					name, err := procName(pid)
+					if err != nil {
+						fmt.Printf("procName err: %v\n", err)
+						continue
+					}
+					res = append(res, ConnProc{
+						Protocol:  "tcp",
+						LocalAddr: tp.localAddress,
+						LocalPort: tp.localPort,
+						PID:       pid,
+						Name:      name,
+					})
+				}
 			}
 		}
 	}
@@ -48,7 +57,8 @@ func Spy() []ConnProc {
 }
 
 func walkProcPid() (map[uint64]uint, error) {
-	// Walk over all /proc entries (numerical ones, those are PIDs), and see if their ./fd/* files link to 'socket[...]' 'files'.
+	// Walk over all /proc entries (numerical ones, those are PIDs), and see if
+	// their ./fd/* files link to 'socket[...]' 'files'.
 	// Returns a map from socket id to pid
 	fh, err := os.Open(procRoot)
 	if err != nil {
@@ -67,12 +77,10 @@ func walkProcPid() (map[uint64]uint, error) {
 			// Not a number, so not a pid subdir.
 			continue
 		}
-		// fmt.Printf("Pid: %v\n", pid)
 
 		dfh, err := os.Open(procRoot + "/" + dirName + "/fd")
 		if err != nil {
 			// process can be gone by now, or we don't have access.
-			// fmt.Printf("Skip %v: %v\n", pid, err)
 			continue
 		}
 		fds, err := dfh.Readdir(-1)
@@ -84,7 +92,6 @@ func walkProcPid() (map[uint64]uint, error) {
 			if procFd.Mode()&os.ModeSymlink == 0 {
 				continue
 			}
-			// fmt.Printf(" Proc: %v: %v\n", pid, procFd.Name())
 
 			// We want sockets only
 			stat, err := os.Stat(procRoot + "/" + dirName + "/fd/" + procFd.Name())
@@ -94,31 +101,11 @@ func walkProcPid() (map[uint64]uint, error) {
 			if stat.Mode()&os.ModeSocket == 0 {
 				continue
 			}
-			// fmt.Printf(" Stat: %v : %v\n", pid, stat.Name())
 			sys, ok := stat.Sys().(*syscall.Stat_t)
 			if !ok {
 				panic("Weird result from stat.Sys()")
 			}
-			// fmt.Printf(" Inode: %v : %v\n", pid, sys.Ino)
 			procmap[sys.Ino] = pid
-
-			/*
-				linkName, err := os.Readlink(procRoot + "/" + pid + "/fd/" + procFd.Name())
-				if err != nil {
-					fmt.Printf("Readlink err: %v\n", err)
-					continue
-				}
-				fmt.Printf(" Link: %v: %v\n", pid, linkName)
-			*/
-			/*
-				eval, err := filepath.EvalSymlinks(procRoot + "/" + name + "/fd/" + procFd.Name())
-				if err != nil {
-					fmt.Printf("EvalSymlinks err: %v\n", err)
-					continue
-				}
-				fmt.Printf(" Link: %v: %v\n", name, eval)
-			*/
-			// fmt.Printf(" Proc: %v: %v\n", name, procFd.Name())
 		}
 		dfh.Close()
 	}
@@ -135,50 +122,48 @@ type transport struct {
 	inode         uint64
 }
 
-// parseTransport4 parses /proc/net/{tcp,udp} files
+// parseTransport parses /proc/net/{tcp,udp}{,6} files
 var fieldRe = regexp.MustCompile(`\s+`)
 
-func parseTransport4(r io.Reader) []transport {
+func parseTransport(r io.Reader) []transport {
 	res := []transport{}
 	scanner := bufio.NewScanner(r)
 	for i := 0; scanner.Scan(); i++ {
 		if i == 0 {
-			// fmt.Printf("Header: %s\n", scanner.Text())
 			continue
 		}
-		// fmt.Printf("Line: %s\n", scanner.Text())
-		// Fields are 'sl local_address rem_address st tx_queue rx_queue tr tm->when retrnsmt uid timeout inode <more>'
+		// Fields are:
+		//  'sl local_address rem_address st tx_queue rx_queue tr tm->when retrnsmt uid timeout inode <more>'
 		line := strings.TrimSpace(scanner.Text())
 		fields := fieldRe.Split(line, -1)
 		if len(fields) < 10 {
-			// log.Error("Invalid format")
-			fmt.Printf("Invalid format")
+			// fmt.Printf("Invalid format")
 			continue
 		}
 
-		localAddress, localPort, err := scanAddress4(fields[1])
+		localAddress, localPort, err := scanAddress(fields[1])
 		if err != nil {
-			fmt.Printf("err: %s\n", err)
+			// fmt.Printf("err: %s\n", err)
 			continue
 		}
 
-		remoteAddress, remotePort, err := scanAddress4(fields[2])
+		remoteAddress, remotePort, err := scanAddress(fields[2])
 		if err != nil {
-			fmt.Printf("err: %s\n", err)
+			// fmt.Printf("err: %s\n", err)
 			continue
 		}
 
 		var uid int
 		_, err = fmt.Sscanf(fields[7], "%d", &uid)
 		if err != nil {
-			fmt.Printf("err: %s\n", err)
+			// fmt.Printf("err: %s\n", err)
 			continue
 		}
 
 		var inode uint64
 		_, err = fmt.Sscanf(fields[9], "%d", &inode)
 		if err != nil {
-			fmt.Printf("err: %s\n", err)
+			// fmt.Printf("err: %s\n", err)
 			continue
 		}
 		t := transport{
@@ -195,18 +180,25 @@ func parseTransport4(r io.Reader) []transport {
 	return res
 }
 
-// scanAddress4 parses things like 'A12CF62E:E4D7' to their address/port
-func scanAddress4(in string) (net.IP, uint16, error) {
+// scanAddress parses things like 'A12CF62E:E4D7' to their address/port.
+// Deals with IPv4 and IPv6 addresses.
+// The address part are big endian 32 bit ints, hex encoded. Since net.IP is a
+// byte slice we just decode the hex and flip the bytes in every group of 4.
+func scanAddress(in string) (net.IP, uint16, error) {
 	parts := strings.Split(in, ":")
 	if len(parts) != 2 {
-		return nil, 0, errors.New("invalid addres:port")
+		return nil, 0, errors.New("invalid address:port")
 	}
 
-	// network (big) endian
-	address := make([]byte, 4)
-	_, err := fmt.Sscanf(parts[0], "%2X%2X%2X%2X", &address[3], &address[2], &address[1], &address[0])
+	// network (big) endian. Can be either ipv4 or ipv6
+	address, err := hex.DecodeString(parts[0])
 	if err != nil {
 		return nil, 0, err
+	}
+	// reverse every 4 byte-sequence.
+	for i := 0; i < len(address); i += 4 {
+		address[i], address[i+3] = address[i+3], address[i]
+		address[i+1], address[i+2] = address[i+2], address[i+1]
 	}
 
 	var port uint16
@@ -216,4 +208,22 @@ func scanAddress4(in string) (net.IP, uint16, error) {
 	}
 
 	return net.IP(address), port, err
+}
+
+// procName does a pid->name lookup
+func procName(pid uint) (string, error) {
+	fh, err := os.Open(fmt.Sprintf("/proc/%d/comm", pid))
+	if err != nil {
+		return "err1", err
+	}
+	name := make([]byte, 1024)
+	l, err := fh.Read(name)
+	if err != nil {
+		return "err", err
+	}
+	if l < 2 {
+		return "", nil
+	}
+	// drop trailing "\n"
+	return string(name[:l-1]), nil
 }
